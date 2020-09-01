9c1d8dc61e069b340015e7712fbf9aec57c2a04a
HUmberto <professorhumbertodelgado@gmail.com>
2020-09-01 18:28:03 -0300

    Teste

--- projetofinal/src/main/java/br/com/projetofinal/controller/UsuarioController.java
+++ projetofinal/src/main/java/br/com/projetofinal/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import br.com.projetofinal.dao.UsuarioDAO;
 import br.com.projetofinal.model.Usuario;
 
 @RestController
+@CrossOrigin("*")
 public class UsuarioController {
 	
 	@Autowired
